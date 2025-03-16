b4d4bfc635674283a4add28f35e80367dc55b988
Tsudhonihm <your.email@example.com>
2025-03-16 13:15:26 -0400

    Make adjustments for deploymnent

--- app.py
+++ app.py
@@ -52,5 +52,5 @@ def message():
         return jsonify({'error': str(e)}), 500
 # Run the app
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8082))  # Default to 5000 if PORT is not set
+    port = int(os.environ.get("PORT", 8080))  # Default to 5000 if PORT is not set
     app.run(debug=True, host="0.0.0.0", port=port)
